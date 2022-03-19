d628270ffda028b4ae8123947123e4949f01771b
DenisLapi <denislapidenis@gmail.com>
2022-03-19 20:49:39 +0100

    [feature] Added Modal molecule as a wrapper of Oruga framework modal

---
+++ src/components/molecules/Modal.vue
@@ -0,0 +1,37 @@
+<o-modal
+  scroll="clip"
+  :active.sync="show"
+  :width="width"
+>
+  <slot />
+</o-modal>
+
+<script>
+import { computed } from 'vue'
+export default {
+  name: 'Modal',
+  props: {
+    isVisible: {
+      type: Boolean,
+      default: false
+    },
+    width: {
+      type: Number,
+      default: 640
+    }
+  },
+  setup (props, { emit }) {
+    const show = computed(() => ({
+      get () {
+        return props.isVisible
+      },
+      set (value) {
+        emit('close', value)
+      }
+    }))
+    return {
+      show
+    }
+  }
+}
+</script>
