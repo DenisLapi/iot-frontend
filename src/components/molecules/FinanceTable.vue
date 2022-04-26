86329f6fe6e19401c41798c76f39bc5ac436b1b2
DenisLapi <denislapidenis@gmail.com>
2022-04-27 00:24:27 +0200

    [feature] : Working on UI for crop details page

---
+++ src/components/molecules/FinanceTable.vue
@@ -0,0 +1,109 @@
+<template>
+  <table class="finance-table">
+    <thead>
+    <tr>
+      <th class="finance-table__box-icon"></th>
+      <th>Description</th>
+      <th>Money</th>
+      <th>Date</th>
+      <th>Actions</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td class="finance-table__box-icon">
+        <div class="finance-table__icon">
+          <icon name="trend-up" color="#80b215" />
+        </div>
+      </td>
+      <td>Planting the crop...</td>
+      <td>
+        <span class="finance-table__label finance-table__label--income">$500</span>
+      </td>
+      <td>2022-05-05</td>
+      <td>
+        <Button type="small">Remove</Button>
+      </td>
+    </tr>
+    <tr>
+      <td class="finance-table__box-icon">
+        <div class="finance-table__icon">
+          <icon name="trend-down" color="#ec3636" />
+        </div>
+      </td>
+      <td>Planting the crop...</td>
+      <td>
+        <span class="finance-table__label finance-table__label--expense">$500</span>
+      </td>
+      <td>2022-05-05</td>
+      <td>
+        <Button type="small">Remove</Button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import Icon from '@/components/atoms/Icon'
+import Button from '@/components/atoms/Button'
+
+export default {
+  name: 'FinanceTable',
+  components: {
+    Icon,
+    Button
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.finance-table {
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+  &__box-icon {
+    width: 50px;
+  }
+  &__icon {
+    width: 20px;
+    height: 20px;
+    border-radius: 6px;
+    margin: auto;
+  }
+  &__label {
+    border-radius: 4px;
+    color: white;
+    padding: 2px 4px;
+    &--income {
+      background-color: #27ae60;
+    }
+    &--expense {
+      background-color: #fa4444;
+    }
+  }
+  th, td {
+    padding: 10px;
+  }
+  thead {
+    font-size: 12px;
+  }
+  tbody {
+    font-size: 15px;
+    td {
+      transition: background .5s ease;
+    }
+    tr:hover td {
+      background: #ffffff;
+      &:first-child {
+        border-top-left-radius: 6px;
+        border-bottom-left-radius: 6px;
+      }
+      &:last-child {
+        border-top-right-radius: 6px;
+        border-bottom-right-radius: 6px;
+      }
+    }
+  }
+}
+</style>
--- src/modules/crops/sub-pages/CropDetails.vue
+++ src/modules/crops/sub-pages/CropDetails.vue
@@ -1,13 +1,78 @@
 <template>
-  <h1>Crop details</h1>
+  <div class="crop-details">
+    <div class="crop-details__header">
+      <div>
+        <div class="crop-details__crop-icon">🧅</div>
+      </div>
+      <div class="crop-details__info">
+        <data-group label="Name" value="Sunflower" />
+        <data-group label="Hybrid" value="S320" />
+        <data-group label="Planting date" value="2022-03-03" />
+        <data-group label="Harvesting date" value="2022-08-08" />
+        <data-group label="Invested" value="$350" />
+        <data-group label="Earned" value="$500" />
+      </div>
+    </div>
+    <div class="crop-details__finance-headline mt-50">
+      <h2>Financial details</h2>
+      <Button type="small primary">
+        <Icon name="plus" color="white" />
+        Add more
+      </Button>
+    </div>
+    <finance-table class="mt-20" />
+  </div>
 </template>
 
 <script>
+import Button from '@/components/atoms/Button'
+import DataGroup from '@/components/atoms/DataGroup'
+import FinanceTable from '@/components/molecules/FinanceTable'
+import Icon from '@/components/atoms/Icon'
+
 export default {
-  name: 'CropDetails'
+  name: 'CropDetails',
+  components: {
+    Icon,
+    Button,
+    FinanceTable,
+    DataGroup
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.crop-details {
+  padding: 40px;
+  min-height: 100vh;
+  background-color: #f5f6f8;
+  &__header {
+    padding: 20px;
+    background-color: #ffffff;
+    box-shadow: 1px 1px 20px rgba(163, 173, 192, 0.18);
+    border-radius: 8px;
+    display: grid;
+    grid-template-columns: 25% auto;
+    column-gap: 20px;
+  }
+  &__info {
+    display: grid;
+    grid-template-columns: repeat(2, auto);
+    column-gap: 20px;
+    row-gap: 20px;
+  }
+  &__finance-headline {
+    display: flex;
+    align-items: center;
+    > * {
+      margin-right: 20px;
+    }
+  }
+}
+.mt-20 {
+  margin-top: 20px
+}
+.mt-50 {
+  margin-top: 50px;
+}
 </style>
