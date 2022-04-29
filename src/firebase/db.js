a125d6f09b1b032d49ed83bde3d3d8b42cb9c7f0
DenisLapi <denislapidenis@gmail.com>
2022-04-29 23:58:31 +0200

    [feature] : Basic file setup and store changes

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,15 +1,22 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
 
-// Get a Firestore instance
-export const db = firebase
-  .initializeApp({ projectId: process.env.FIREBASE_PROJECT_ID })
-  .firestore()
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} = process.env
 
-// Export types that exists in Firestore
-// This is not always necessary, but it's used in other examples
-const { Timestamp, GeoPoint } = firebase.firestore
-export { Timestamp, GeoPoint }
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
+}
 
-// if using Firebase JS SDK < 5.8.0
-db.settings({ timestampsInSnapshots: true })
+export const db = firebase.initializeApp(firebaseConfig).firestore()
--- src/modules/crops/store/index.js
+++ src/modules/crops/store/index.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import DEFAULT_CROPS from './data'
+import { db } from '@/firebase/db'
 
 export const useCropStore = defineStore('crop', {
   state: () => ({
@@ -12,7 +12,11 @@ export const useCropStore = defineStore('crop', {
      * Function loads the crops in the store
      */
     loadCrops () {
-      this.crops = DEFAULT_CROPS
+      db.collection('crops')
+        .get()
+        .then(querySnapshot => {
+          this.crops = querySnapshot.docs.map(doc => doc.data())
+        })
     },
 
     /**
@@ -20,8 +24,9 @@ export const useCropStore = defineStore('crop', {
      * @param crop crop details which needs to include crop id
      */
     updateCrop (crop) {
-      const index = this.crops.findIndex(({ id }) => crop.id === id)
-      this.crop[index] = crop
+      db.collection('crops')
+        .doc(crop.id)
+        .set(crop)
     },
 
     /**
@@ -30,8 +35,12 @@ export const useCropStore = defineStore('crop', {
      * @returns {*} Crop
      */
     getCrop (cropId) {
-      const index = DEFAULT_CROPS.findIndex(({ id }) => cropId === id)
-      return DEFAULT_CROPS[index]
+      db.collection('crops')
+        .doc(cropId)
+        .get()
+        .then(querySnapshot => {
+          return querySnapshot.data()
+        })
     },
 
     /**
@@ -39,16 +48,18 @@ export const useCropStore = defineStore('crop', {
      * @param crop
      */
     addCrop (crop) {
-      this.crop.push(crop)
+      db.collection('crops')
+        .add(crop).then(r => r)
     },
 
     /**
      * Function deletes crop from the crops list
      * @param crop Crop object with id property
      */
-    deleteCrop (crop) {
-      const index = this.crops.findIndex(({ id }) => crop.id === id)
-      this.crop.splice(index, 1)
+    deleteCrop ({ id }) {
+      db.collection('crops')
+        .doc(id)
+        .delete()
     },
 
     /**
